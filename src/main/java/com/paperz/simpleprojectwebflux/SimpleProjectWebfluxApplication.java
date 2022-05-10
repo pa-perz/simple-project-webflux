744b595f27381a51d5a7ebd17c4d66d70a3db6d7
pperez <pa_perz@outlook.es>
2022-05-10 22:10:06 +0100

    Initial commit

---
+++ src/main/java/com/paperz/simpleprojectwebflux/SimpleProjectWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.paperz.simpleprojectwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleProjectWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleProjectWebfluxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paperz/simpleprojectwebflux/SimpleProjectWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paperz.simpleprojectwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleProjectWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
